ab914e217323bd50309ed1af212ef7ae8e4e2d26
Arahndir27 <espressler18325@gmail.com>
2022-11-12 14:47:28 -0700

    Refactor quiz.dart to be stateful

--- lib/zquiz.dart
+++ lib/zquiz.dart
@@ -4,32 +4,27 @@ import 'package:flutter/material.dart';
 
 import 'question.dart';
 
-class Quiz extends StatefulWidget {
+class Quiz extends StatelessWidget {
+  @required
   var _quizName;
   final List<Question> _questions;
-
-  Quiz(this._quizName, this._questions);
-
-  @override
-  State<Quiz> createState() => _QuizState();
-}
-
-class _QuizState extends State<Quiz> {
-  // var _quizName;
-  // late final List<Question> _questions;
   var _questionIndex = 0;
   var _numCorrect = 0;
+
+  //Constructor
+  Quiz(this._quizName, this._questions);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          widget._quizName,
+          _quizName,
         ),
       ),
       body: Center(
-        child: widget._questions[_questionIndex], //TODO: iterate over questions so go one at a time
+        child: _questions[
+            _questionIndex], //TODO: iterate over questions so go one at a time
       ),
     );
   }
